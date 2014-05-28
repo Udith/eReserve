d5fbbd53179f82091e4e10ea322ae5c39bb84461
Udith <uditharosha@gmail.com>
2014-05-29 01:14:11 +0530

    Calendar table row selection added

--- js/er_calendar.js
+++ js/er_calendar.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 
-    $('#calendar-table').dataTable({
+    var table = $('#calendar-table').dataTable({
         "columns": [
             {"data": "id"},
             {"data": "name"},
@@ -12,17 +12,14 @@ $(document).ready(function() {
         ],
         "columnDefs": [
             {
-                // The `data` parameter refers to the data for the cell (defined by the
-                // `data` option, which defaults to the column being worked with, in
-                // this case `data: 0`.
-                "render": function ( data, type, row ) {
-                    if(data === true){
+                "render": function(data, type, row) {
+                    if (data === true) {
                         return "<span class='glyphicon glyphicon-ok'></span>";
-                    }else{
+                    } else {
                         return "<span class='glyphicon glyphicon-remove'></span>";
                     }
                 },
-                "targets": [5,6]
+                "targets": [5, 6]
             }
         ],
         "ajax": {
@@ -30,16 +27,11 @@ $(document).ready(function() {
             "type": "POST",
             "dataSrc": "halls"
         }
-        
+
     });
-//    $("#calendar-table tfoot th").each(function(i) {
-//        var select = $('<select><option value=""></option></select>');
-//        select.appendTo($(this).empty()).on('change', function() {
-//            table.column(i).search($(this).val()).draw();
-//        });
-//
-//        $("#calendar-table tfoot th").column(i).data().unique().sort().each(function(d, j) {
-//            select.append('<option value="' + d + '">' + d + '</option>');
-//        });
-//    });
+    
+    table.on( 'click', 'tr', function () {
+        console.log($(this).find("td").first().html());
+        $('#calendar-modal').modal('show');
+    } );
 });
